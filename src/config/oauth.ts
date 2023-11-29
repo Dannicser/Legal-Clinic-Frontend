df27adab2716af82d82b8186467670f92347e2c4
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2023-11-29 20:24:09 +0300

    delete later!

---
+++ src/config/oauth.ts
@@ -0,0 +1,4 @@
+export const client_id = "39d43ae5588845ea807b5c6343b1f745";
+export const client_secret = "137a62a7a483439e8c4d8cccf306fce9";
+export const url_get_token = "https://oauth.yandex.ru/token";
+export const url_get_data = "https://login.yandex.ru/info";
