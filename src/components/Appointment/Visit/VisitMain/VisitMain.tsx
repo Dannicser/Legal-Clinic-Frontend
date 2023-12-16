3363406bfe138f6a3664eaa1227d542ba23c6065
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2023-12-16 14:51:40 +0300

    fix compiling problem

--- src/components/Appointment/Visit/VisitMain/VisitMain.tsx
+++ src/components/Appointment/Visit/VisitMain/VisitMain.tsx
@@ -13,7 +13,7 @@ export const VisitMain: React.FC = () => {
   const status = useAppSelector((state) => state.appointment.data.status);
   const message = useAppSelector((state) => state.appointment.message);
 
-  const statusComponentMapper: Record<Status, React.ReactElement> = {
+  const statusComponentMapper: Record<Status, React.ReactElement<any>> = {
     none: <VisitForm />,
     accepted: <Accepted />,
     rejected: <Rejected message={message} />,
@@ -22,5 +22,5 @@ export const VisitMain: React.FC = () => {
     provided: <Provided />,
   };
 
-  return <>{statusComponentMapper[status] as any}</>;
+  return <>{statusComponentMapper[status]}</>;
 };
