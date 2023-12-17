812cee4cc6b61995b280ec86d5016552f236f289
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2023-12-17 13:31:39 +0300

    fix problems

--- src/http/vars.ts
+++ src/http/vars.ts
@@ -1,4 +1,4 @@
 const mode = process.env.NODE_ENV;
 
-export const BACKEND_URL = mode === "development" ? "http://localhost:5000/api" : "http://45.147.178.70:5000/api";
-export const FRONTEND_URL = mode === "development" ? "http://localhost:3000" : "http://45.147.178.70:80";
+export const BACKEND_URL = mode === "development" ? "http://localhost:5000/api" : "https://legal-clinic:5000/api";
+export const FRONTEND_URL = mode === "development" ? "http://localhost:3000" : "https://legal-clinic:80";
